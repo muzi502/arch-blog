8420935d7fc55964af459db070c29bb63d18c2ef
muzi <git@502.li>
2020-03-22 15:18:56 +0800

    update delpoy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,15 +9,12 @@ hexo_dir=$(pwd)
 post_dir=${hexo_dir}/source/_posts
 public_dir=${hexo_dir}/public
 cd ${hexo_dir}
-git pull
 cd ${post_dir}
 rename 's/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])-)//' *.md
 
 cd ${hexo_dir}
 rm -rf db.json public/*
-mv ${hexo_dir}/source/img ${public_dir}/
 hexo g
-cp -rf ${public_dir}/img ${hexo_dir}/source/
 rm -rf  ${post_dir}
 git checkout ${post_dir}
 find ${public_dir} -name '*.html' -type f -print0 | xargs -0 sed -i '/^[[:space:]]*$/d'
